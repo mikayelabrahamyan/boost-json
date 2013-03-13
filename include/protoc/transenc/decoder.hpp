5b69b839f7ac9ca307be87ada6495acb888fd073
Bjorn Reese <breese@sourceforge.net>
2013-03-13 21:48:04 +0100

    Initial transenc decoder

---
+++ include/protoc/transenc/decoder.hpp
@@ -0,0 +1,66 @@
+#ifndef PROTOC_TRANSENC_DECODER_HPP
+#define PROTOC_TRANSENC_DECODER_HPP
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// http://protoc.sourceforge.net/
+//
+// Copyright (C) 2013 Bjorn Reese <breese@users.sourceforge.net>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+// MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE AUTHORS AND
+// CONTRIBUTORS ACCEPT NO RESPONSIBILITY IN ANY CONCEIVABLE MANNER.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <string>
+#include <protoc/types.hpp>
+#include <protoc/input_range.hpp>
+#include <protoc/transenc/token.hpp>
+
+namespace protoc
+{
+namespace transenc
+{
+
+class decoder
+{
+public:
+    decoder(const char *begin, const char *end);
+
+    token next();
+
+    protoc::int8_t get_int8() const;
+    protoc::int16_t get_int16() const;
+    protoc::int32_t get_int32() const;
+    protoc::int64_t get_int64() const;
+    std::string get_string() const;
+
+private:
+    token next_int8();
+    token next_int16();
+    token next_int32();
+    token next_int64();
+    token next_string();
+
+    token next_unknown();
+    token next_unknown(std::size_t);
+
+private:
+    input_range input;
+    struct
+    {
+        token type;
+        input_range range;
+    } current;
+};
+
+}
+}
+
+#endif /* PROTOC_TRANSENC_DECODER_HPP */
