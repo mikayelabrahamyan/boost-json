c91d4a05fe3d4b7540ea68f441a7cc1d349bcd26
Bjorn Reese <breese@sourceforge.net>
2013-03-29 00:12:42 +0100

    Removed unnecesary function signatures

--- include/protoc/json/encoder.hpp
+++ include/protoc/json/encoder.hpp
@@ -34,11 +34,7 @@ public:
 
     std::size_t put(); // Null
     std::size_t put(bool);
-    std::size_t put(protoc::int8_t);
-    std::size_t put(protoc::int16_t);
-    std::size_t put(protoc::int32_t);
     std::size_t put(protoc::int64_t);
-    std::size_t put(protoc::float32_t);
     std::size_t put(protoc::float64_t);
     std::size_t put(const char *);
     std::size_t put(const std::string&);
