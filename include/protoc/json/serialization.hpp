a96fbf03fdc4973f26e3acd38bb192b30e44ff22
Bjorn Reese <breese@sourceforge.net>
2014-02-10 12:20:33 +0100

    Added license

--- include/protoc/json/serialization.hpp
+++ include/protoc/json/serialization.hpp
@@ -1,6 +1,18 @@
 #ifndef PROTOC_JSON_SERIALIZATION_HPP
 #define PROTOC_JSON_SERIALIZATION_HPP
 
+///////////////////////////////////////////////////////////////////////////////
+//
+// http://protoc.sourceforge.net/
+//
+// Copyright (C) 2014 Bjorn Reese <breese@users.sourceforge.net>
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #include <protoc/json/iarchive.hpp>
 #include <protoc/json/oarchive.hpp>
 #include <protoc/serialization/serialization.hpp>
